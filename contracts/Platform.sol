cee6170952f4a89414998e5d44b89ec16b47d137
apajaresaguilera <apajaresaguilera@gmail.com>
2022-01-12 08:26:21 +0100

    initial commit

---
+++ contracts/Platform.sol
@@ -0,0 +1,22 @@
+//SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.0;
+
+import "hardhat/console.sol";
+
+contract Platform {
+    address[] projects;
+    mapping (address =>  address[]) walletToAccountsFunded;
+
+
+    constructor (){
+        
+    }
+
+    
+
+
+
+
+
+
+}
